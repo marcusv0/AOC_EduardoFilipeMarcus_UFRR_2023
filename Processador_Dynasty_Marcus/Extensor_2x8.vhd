0b9dccb9a76f59b02925bd2d8e1f2b7a44129427
Marcus Vinicius <93393783+marcusv0@users.noreply.github.com>
2023-11-28 16:51:14 -0400

    Update Extensor_2x8.vhd

--- Processador_Dynasty_Marcus/Extensor_2x8.vhd
+++ Processador_Dynasty_Marcus/Extensor_2x8.vhd
@@ -1,19 +1,21 @@
+---_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_---
+-- COMPONENTE: EXTENSOR_2X8
+-- DESCRIÇÃO: 
+--     EXTENDE UM SINAL DE 2 BITS PARA 1 BYTE
+---_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_---
+
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY EXTENSOR_2X8 IS
-    PORT(
-        A : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-        S : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
-    );
+	PORT(
+		A : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+		S : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+	);
 END EXTENSOR_2X8;
 
 ARCHITECTURE BEHAVIOR OF EXTENSOR_2X8 IS
 BEGIN
-    PROCESS(A)
-    BEGIN
-        -- Ajuste para evitar que as saídas fiquem fixadas em GND
-        S(7 DOWNTO 2) <= (OTHERS => '0');
-        S(1 DOWNTO 0) <= A(3 DOWNTO 2);
-    END PROCESS;
-END ARCHITECTURE;
+	S(7 DOWNTO 2) <= (OTHERS => '0');
+	S(1 DOWNTO 0) <= A(3 DOWNTO 2);
+END;
